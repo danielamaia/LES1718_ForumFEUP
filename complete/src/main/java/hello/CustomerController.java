d53a9b488277c99afebdc6135189079c53a0734a
Daniela <up201709690@fe.up.pt>
2017-10-31 19:03:44 +0000

    wip código base

--- complete/src/main/java/hello/CustomerController.java
+++ complete/src/main/java/hello/CustomerController.java
@@ -6,18 +6,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import javax.inject.Inject;
 
 @RestController
 public class CustomerController {
+
+    @Inject
+    private CustomerRepository customerRepository;
 
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
 
-    @RequestMapping(value = "/customer", method = RequestMethod.GET)
-    public ResponseEntity<String> getCostumerById(){
-        return new ResponseEntity<String>("a", HttpStatus.OK);
+    @RequestMapping(value = "/customer/{id}", method = RequestMethod.GET)
+    public ResponseEntity<String> getCostumerById(@PathVariable Long id){
+        String customer = customerRepository.findOne(id).getLastName();
+        return new ResponseEntity<String>(customer, HttpStatus.OK);
     }
 
 }
