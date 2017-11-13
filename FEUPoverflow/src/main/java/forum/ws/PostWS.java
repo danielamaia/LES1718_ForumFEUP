14126a87669527f93c4578f3f9e2d98c1b4f04a1
Daniela <up201709690@fe.up.pt>
2017-11-13 21:33:56 +0000

    post layout

--- FEUPoverflow/src/main/java/forum/ws/PostWS.java
+++ FEUPoverflow/src/main/java/forum/ws/PostWS.java
@@ -2,20 +2,29 @@ package forum.ws;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import forum.domain.Post;
-import forum.service.*;
+import forum.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.inject.Inject;
 import java.io.IOException;
 
-@RestController
+@Controller
 public class PostWS {
 
     @Inject
     private PostService postService;
     ObjectMapper objectMapper = new ObjectMapper();
+
+    @RequestMapping("/post")
+    public String index() {
+        return "post";
+    }
 
     @RequestMapping(value = "/post/{id}", method = RequestMethod.GET)
     public ResponseEntity<Post> getPostById(@PathVariable Long id){
