582dd68b1c07236d93cfc7d7b2954d17044b51a9
up201304360 <ines_ruud@hotmail.com>
2017-12-10 19:26:02 +0000

    Update Student.java

--- FEUPoverflow/src/main/java/forum/domain/Student.java
+++ FEUPoverflow/src/main/java/forum/domain/Student.java
@@ -26,6 +26,15 @@ public class Student {
 	public String getLastName() {
 		return lastName;
 	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public void setLastName(String lastName) {
+       		 this.lastName = lastName;
+    }
+
+
 
 }
 
